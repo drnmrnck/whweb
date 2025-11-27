01c48a635104bc43e9e2ee31b77599149d3d23e1
drnmrnck <mironcukdarina43@gmail.com>
2025-11-27 16:21:33 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,12 +2,13 @@ let hamburger = document.querySelector('.hamburger');
 let nav = document.querySelector('.nav');
 
 hamburger.addEventListener('click', function(e) {
-	e.stopPropagation();
-	nav.classList.toggle('active');
+    e.stopPropagation();
+    nav.classList.toggle('active');
 });
 
 document.addEventListener('click', function(e) {
-	if (!nav.contains(e.target)) {
-		nav.classList.remove('active');
-	}
+    if (!nav.contains(e.target) && !hamburger.contains(e.target)) {
+        nav.classList.remove('active');
+    }
 });
+
